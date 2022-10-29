57ddc82efc9f2938cf7ef6d9445427254058cdee
Christopher R. Palmer <crpalmer@gmail.com>
2022-10-29 13:18:58 -0400

    2022

--- app/src/main/java/org/dooey/halloween/controls/MainActivity.java
+++ app/src/main/java/org/dooey/halloween/controls/MainActivity.java
@@ -26,21 +26,22 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		Remote animationStationRemote = new Remote("animation-station.dooey.org", 5555);
-		Remote bandRemote = new Remote("band.dooey.org", 5555);
-		Remote foggerRemote = new Remote("talker.dooey.org", 5556);
-		Remote talkerRemote = new Remote("talker.dooey.org", 5555);
+		//Remote bandRemote = new Remote("band.dooey.org", 5555);
+		//Remote foggerRemote = new Remote("talker.dooey.org", 5556);
+		//Remote talkerRemote = new Remote("talker.dooey.org", 5555);
 
-		createPropAction(animationStationRemote, R.id.button1, "snake");
-		createPropAction(animationStationRemote, R.id.button2, "bunny");
-		createPropAction(animationStationRemote, R.id.button3, "M&M");
-		createPropAction(animationStationRemote, R.id.button4, "demono");
-		createPropAction(foggerRemote, R.id.fog_button, "fog");
-		createPropAction(foggerRemote, R.id.more_button, "duty_up");
-		createPropAction(foggerRemote, R.id.less_button, "duty_down");
-		createPropAction(bandRemote, R.id.band, "play");
+		createPropAction(animationStationRemote, R.id.button1, "mosquito");
+		createPropAction(animationStationRemote, R.id.button2, "gater");
+		createPropAction(animationStationRemote, R.id.button3, "question");
+		createPropAction(animationStationRemote, R.id.button4, "spider");
+		createPropAction(animationStationRemote, R.id.button5, "snake");
+		//createPropAction(foggerRemote, R.id.fog_button, "fog");
+		//createPropAction(foggerRemote, R.id.more_button, "duty_up");
+		//createPropAction(foggerRemote, R.id.less_button, "duty_down");
+		//createPropAction(bandRemote, R.id.band, "play");
 
-		final Button speakButton = findViewById(R.id.speak_button);
-		new RecordButton(speakButton, new RecordingWork(
-				new RemotePublisher(this, speakButton, talkerRemote, "play")));
+		//final Button speakButton = findViewById(R.id.speak_button);
+		//new RecordButton(speakButton, new RecordingWork(
+		//		new RemotePublisher(this, speakButton, talkerRemote, "play")));
 	}
 }
